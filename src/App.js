5c1101665d097a2b03bee3f3827fadc916a5f96a
Nikhil Vikraman <nikhil.v@neoito.com>
2023-01-05 12:09:01 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TodoItem from './new_block_2'
+
+export default function App(props) {
+  return (
+    <div>
+      <TodoItem item="sdfsdf" id="sdfsdf" />
+    </div>
+  )
+}
